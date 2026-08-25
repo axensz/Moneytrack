30db469a47cbe37b7a21b3fe80818f50eee9f684
Camilo Guzman <camilo.guzman@pragma.com>
2026-08-24 21:42:27 -0500

    chore: refresh service worker cache version

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // Service Worker for MoneyTrack PWA
 // Bump CACHE_VERSION on each deploy to invalidate old caches
-const CACHE_VERSION = 'v3-d3cb6fe';
+const CACHE_VERSION = 'v3-5591401';
 const APP_BASE_PATH = (() => {
     const scopePath = new URL(self.registration.scope).pathname.replace(/\/$/, '');
     return scopePath === '' ? '' : scopePath;
